a6616886dfa40b9d0e2b7ddaee9c4631d4b6d6a5
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2015-07-24 18:10:46 +0530

    Deploying now

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git add .
 git commit -a -m 'Deploying now'
 git push -u origin master
-ssh ~/aws/sarawagi/sarawagi_ec2.pem ubuntu@52.3.212.51
+ssh -i ~/aws/sarawagi/sarawagi_ec2.pem ubuntu@52.3.212.51
 cd ./the-exam
 git pull
 export GOPATH=/home/ubuntu/the-exam/server
